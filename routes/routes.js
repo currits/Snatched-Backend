dc1bac71cff52b463ef74833cd0f44da7a1f2fcf
Espi <ethyn.gillies@gmail.com>
2023-09-27 12:44:03 +1300

    editUser controller added Allows user editing for all fields

--- routes/routes.js
+++ routes/routes.js
@@ -3,12 +3,14 @@ const router = express.Router();
 const { login, signup } = require('../controllers/auth.js');
 const { createListing } = require('../controllers/createListing.js');
 const { verifyToken } = require('../middleware/auth.js');
+const { editUser } = require('../controllers/userController.js');
 
-// User Auth
+/// User Auth
 router.post("/login", login);
 router.post("/signup", signup);
 
-// Auth required routes
+/// Auth required routes
 router.post("/listing/create", verifyToken, createListing);
+router.put("/user/edit", verifyToken, editUser);
 
 module.exports = router;
