4178823febf63763b743ec1e8dfec262080854a8
Espi <ethyn.gillies@gmail.com>
2023-09-27 15:32:09 +1300

    getUser controller implemented

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { login, signup } = require('../controllers/auth.js');
 const { createListing } = require('../controllers/createListing.js');
 const { verifyToken } = require('../middleware/auth.js');
-const { editUser } = require('../controllers/userController.js');
+const { editUser, getUser } = require('../controllers/userController.js');
 const listings = require("../controllers/listing.controller.js");
 
 /// User Auth
@@ -15,5 +15,6 @@ router.get("/listing/one", listings.getOne);
 /// Auth required routes
 router.post("/listing/create", verifyToken, createListing);
 router.put("/user/edit", verifyToken, editUser);
+router.post("/user/get", verifyToken, getUser);
 
 module.exports = router;
