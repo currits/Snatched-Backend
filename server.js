7f179041a35b11acfb9199eb86c944ed4290fb3c
Espi <ethyn.gillies@gmail.com>
2023-09-28 20:26:36 +1300

    Https added! certs not included obviously...

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const helmet = require('helmet');
 const express = require('express');
 const router = require('./routes/routes.js')
 const db = require('./models');
+const https = require('https');
+const fs = require('fs');
 
 const server = express();
 
 // Enable Cloudflare HTTPS
 // From: https://saturncloud.io/blog/how-to-use-express-cloudflare-with-ssl-for-secure-web-applications/#step-4-update-express-app
-server.enable('trust proxy');
+//server.enable('trust proxy');
 
 // Basic security
 server.use(helmet());
@@ -38,5 +40,19 @@ db.tag.belongsToMany(db.listing, { through: 'ListingTags' });
 // ALTER: TRUE WILL MESS WITH CHANGED TABLES
 //db.sequelize.sync({ alter: true });
 
-console.log("Server listening on port: " + process.env.PORT);
-server.listen(process.env.PORT);
+const privateKey = fs.readFileSync('./certs/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('./certs/cert.pem', 'utf8');
+const ca = fs.readFileSync('./certs/chain.pem', 'utf8');
+
+const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca
+};
+
+// Starting https server
+const httpsServer = https.createServer(credentials, server);
+
+httpsServer.listen(process.env.PORT, () => {
+    console.log("Server listening on port: " + process.env.PORT);
+});
