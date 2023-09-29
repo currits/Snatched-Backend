6f3b41b87102a2f8e3f8433f78c9af0a4aae4c86
Espi <ethyn.gillies@gmail.com>
2023-09-29 16:46:11 +1300

    Fixed auth token declared in try statement, and extended jwt signing time to DANGEROUS levels

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,11 +6,13 @@ const jwt = require("jsonwebtoken");
 */
 const verifyToken = (req, res, next) => {
 
+    let token;
+
     if (!req.headers.authorization)
         return res.status(403).send("Missing token");
 
     try {
-        const token = req.headers.authorization.split(' ')[1];
+        token = req.headers.authorization.split(' ')[1];
     }
     catch (err) {
         console.error(err);
